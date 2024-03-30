eb06ef0238a08b2c386bb190bed57379249a518a
ryan <trongdv@coccoc.com>
2024-03-30 11:27:57 +0700

    feat: add process props attribute

--- packages/core/src/diff/props.js
+++ packages/core/src/diff/props.js
@@ -1,9 +1,10 @@
 import { options } from '../options'
+import { isFunction } from '../shared/is'
 
 export function setProperty(dom, name, value, oldValue, isSvg) {
   let useCapture;
 
-  if (name === 'style') {
+  o: if (name === 'style') {
     if (typeof value === 'string') {
       dom.style.cssText = value;
     }
@@ -19,7 +20,7 @@ export function setProperty(dom, name, value, oldValue, isSvg) {
     else name = name.slice(2);
 
     if (!dom._listeners) dom._listeners = {};
-		dom._listeners[name + useCapture] = value;
+    dom._listeners[name + useCapture] = value;
 
     if (value) {
       if (!oldValue) {
@@ -29,6 +30,42 @@ export function setProperty(dom, name, value, oldValue, isSvg) {
     } else {
       const handler = useCapture ? eventProxyCapture : eventProxy;
       dom.removeEventListener(name, handler, useCapture);
+    }
+  } else {
+    if (isSvg) {
+      // Normalize incorrect prop usage for SVG:
+      // - xlink:href / xlinkHref --> href (xlink:href was removed from SVG and isn't needed)
+      // - className --> class
+      name = name.replace(/xlink(H|:h)/, 'h').replace(/sName$/, 's');
+    } else if (
+      name !== 'width' &&
+      name !== 'height' &&
+      name !== 'href' &&
+      name !== 'list' &&
+      name !== 'form' &&
+      // Default value in browsers is `-1` and an empty string is
+      // cast to `0` instead
+      name !== 'tabIndex' &&
+      name !== 'download' &&
+      name !== 'rowSpan' &&
+      name !== 'colSpan' &&
+      name !== 'role' &&
+      name in dom
+    ) {
+      try {
+        dom[name] = value == null ? '' : value;
+        // labelled break is 1b smaller here than a return statement (sorry)
+        break o;
+      } catch (e) { }
+    }
+
+    // aria- and data- attributes
+    if (isFunction(value)) {
+      // ignore
+    } else if (value != null && (value !== false && name[4] === '-')) {
+      dom.setAttribute(name, value)
+    } else {
+      dom.removeAttribute(name)
     }
   }
 }
@@ -40,24 +77,24 @@ export function setProperty(dom, name, value, oldValue, isSvg) {
  * @private
  */
 function eventProxy(e) {
-	if (this._listeners) {
-		const eventHandler = this._listeners[e.type + false];
-		/**
-		 * This trick is inspired by Vue https://github.com/vuejs/core/blob/main/packages/runtime-dom/src/modules/events.ts#L90-L101
-		 * when the dom performs an event it leaves micro-ticks in between bubbling up which means that an event can trigger on a newly
-		 * created DOM-node while the event bubbles up, this can cause quirky behavior as seen in https://github.com/preactjs/preact/issues/3927
-		 */
-		if (!e._dispatched) {
-			// When an event has no _dispatched we know this is the first event-target in the chain
-			// so we set the initial dispatched time.
-			e._dispatched = Date.now();
-			// When the _dispatched is smaller than the time when the targetted event handler was attached
-			// we know we have bubbled up to an element that was added during patching the dom.
-		} else if (e._dispatched <= eventHandler._attached) {
-			return;
-		}
-		return eventHandler(options.event ? options.event(e) : e);
-	}
+  if (this._listeners) {
+    const eventHandler = this._listeners[e.type + false];
+    /**
+     * This trick is inspired by Vue https://github.com/vuejs/core/blob/main/packages/runtime-dom/src/modules/events.ts#L90-L101
+     * when the dom performs an event it leaves micro-ticks in between bubbling up which means that an event can trigger on a newly
+     * created DOM-node while the event bubbles up, this can cause quirky behavior as seen in https://github.com/preactjs/preact/issues/3927
+     */
+    if (!e._dispatched) {
+      // When an event has no _dispatched we know this is the first event-target in the chain
+      // so we set the initial dispatched time.
+      e._dispatched = Date.now();
+      // When the _dispatched is smaller than the time when the targetted event handler was attached
+      // we know we have bubbled up to an element that was added during patching the dom.
+    } else if (e._dispatched <= eventHandler._attached) {
+      return;
+    }
+    return eventHandler(options.event ? options.event(e) : e);
+  }
 }
 
 /**
@@ -66,7 +103,7 @@ function eventProxy(e) {
  * @private
  */
 function eventProxyCapture(e) {
-	if (this._listeners) {
-		return this._listeners[e.type + true](options.event ? options.event(e) : e);
-	}
+  if (this._listeners) {
+    return this._listeners[e.type + true](options.event ? options.event(e) : e);
+  }
 }
